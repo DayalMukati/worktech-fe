39a81a79cb156b1646ba6d03977e5a1cd4cf07a2
agni-c <agnibha0016@gmail.com>
2024-06-21 11:11:22 +0530

    refactor: Remove sign-in page and replace with full-screen login component

--- src/app/auth/log-in/page.jsx
+++ src/app/auth/log-in/page.jsx
@@ -1,10 +1,11 @@
+import LoginFullScreen from '@/components/login-full-screen';
 import { LoginForm } from '@/components/user-login-form';
 import React from 'react';
 
 const SignInPage = () => {
 	return (
-		<div className='flex justify-center items-center w-full min-h-screen'>
-			<LoginForm />
+		<div className='flex justify-center items-center w-full'>
+			<LoginFullScreen />
 		</div>
 	);
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Dashboard from '@/components/dashboard';
+import LoginFullScreen from '@/components/login-full-screen';
 import React from 'react';
 
 const main = () => {
 	return (
 		<div>
-			<Dashboard />
+			<LoginFullScreen />
 		</div>
 	);
 };
