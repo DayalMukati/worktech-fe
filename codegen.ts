d35531ecf46be96ca30bd84e9d85be3e5282b7b2
agni-c <agnibha0016@gmail.com>
2024-06-25 04:55:02 +0530

    chore: Update package.json with graphql-codegen scripts

---
+++ codegen.ts
@@ -0,0 +1,27 @@
+//https://www.apollographql.com/docs/react/development-testing/static-typing/
+//https://github.com/dotansimha/graphql-code-generator/issues/1757#issuecomment-1436039415
+
+/* eslint-disable no-process-env */
+import { CodegenConfig } from '@graphql-codegen/cli';
+import { loadEnvConfig } from '@next/env';
+
+// @ts-ignore
+loadEnvConfig(process.cwd());
+
+const config: CodegenConfig = {
+	schema: process.env.NEXT_PUBLIC_GQL_SERVER,
+	// this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
+	documents: ['graphql/**/*.{ts,tsx}'],
+	generates: {
+		'./src/graphql/__generated__/': {
+			preset: 'client',
+			plugins: [],
+			presetConfig: {
+				gqlTagName: 'gql'
+			}
+		}
+	},
+	ignoreNoDocuments: true
+};
+
+export default config;
