fa99014ee16c086f117e158aa284b54b00b9e207
agni-c <agnibha0016@gmail.com>
2024-06-25 15:34:39 +0530

    refactor: Add registerUser mutation to mutation.ts

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -1,5 +1,6 @@
 import { gql } from './__generated__';
 
+// common
 export const LOGIN_USER_WITH_WALLET = gql(`
   mutation loginUser($walletAddress: String!) {
     loginUser(input: { walletAddress: $walletAddress }) {
@@ -11,6 +12,26 @@ export const LOGIN_USER_WITH_WALLET = gql(`
         skills {
           _id
           title
+        }
+        firstName
+        lastName
+        status
+      }
+      token
+    }
+  }
+`);
+
+export const REGISTER_USER = gql(`
+  mutation registerUser($input: CreateUserInput!) {
+    registerUser(input: $input) {
+      user {
+        _id
+        userRoles {
+          _id
+        }
+        skills {
+          _id
         }
         firstName
         lastName
