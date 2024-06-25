dd056559ef012fa2edbad756cbf250699fd5ce8a
Hpatwareids <154405565+Hpatwareids@users.noreply.github.com>
2024-06-25 17:26:09 +0530

    taskdetails

--- src/app/orgs/org-overview/leaderboard/page.tsx
+++ src/app/orgs/org-overview/leaderboard/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import TopContributor from "@/components/TopContributors";
 import TopReviewer from "@/components/TopReviewer";
 import { MailWarningIcon } from "lucide-react";
-import { data1 } from '@/data/data';
-import { data2 } from '@/data/data';
+import { data1 } from '@/conf/data';
+import { data2 } from '@/conf/data';
 function Leaderboard() {
   return (
     <>
