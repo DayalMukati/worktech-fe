a6bb1facc09b6b91525a4f65879797047e2cbb3a
agni-c <agnibha0016@gmail.com>
2024-06-25 13:08:08 +0530

    refactor: Update import paths for data files in components

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -3,14 +3,17 @@ import { store } from '@/store';
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { TooltipProvider } from '@radix-ui/react-tooltip';
+import GQLProvider from './graphql-provider';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<>
 			<ReduxProvider store={store}>
-				<TooltipProvider skipDelayDuration={50} delayDuration={80}>
-					{children}
-				</TooltipProvider>
+				<GQLProvider>
+					<TooltipProvider skipDelayDuration={50} delayDuration={80}>
+						{children}
+					</TooltipProvider>
+				</GQLProvider>
 			</ReduxProvider>
 		</>
 	);
