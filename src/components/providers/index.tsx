262815ae58d7eab88ff56a9989b5aa75b400c449
agni-c <agnibha0016@gmail.com>
2024-06-26 19:47:47 +0530

    chore: Add @web3-react/core dependency and Web3ConnectProvider component

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -4,16 +4,21 @@ import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { TooltipProvider } from '@radix-ui/react-tooltip';
 import GQLProvider from './graphql-provider';
+import Web3ConnectProvider from './web3-provider';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<>
 			<ReduxProvider store={store}>
-				<GQLProvider>
-					<TooltipProvider skipDelayDuration={50} delayDuration={80}>
-						{children}
-					</TooltipProvider>
-				</GQLProvider>
+				<Web3ConnectProvider>
+					<GQLProvider>
+						<TooltipProvider
+							skipDelayDuration={50}
+							delayDuration={80}>
+							{children}
+						</TooltipProvider>
+					</GQLProvider>
+				</Web3ConnectProvider>
 			</ReduxProvider>
 		</>
 	);
