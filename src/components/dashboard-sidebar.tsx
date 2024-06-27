c8a231d3109b4b1a1dfa760327c463d72034058a
anuragids <akharwar@idssoft.com>
2024-06-27 14:51:42 +0530

    removing duplicate compoents

--- src/components/dashboard-sidebar.tsx
+++ src/components/dashboard-sidebar.tsx
@@ -25,30 +25,31 @@ const DashboardSidebar = () => {
 		}
 	];
 	return (
-		<aside className='col-span-1 bg-background border-r'>
-			<div className='p-3.5 border-b-2'>
-				<h2 className='text-xl'>Dashboard</h2>
-			</div>
-			<nav className='p-4'>
-				<ul className='space-y-2'>
-					{menuItems.map(({ href, icon, label }) => (
-						<li>
-							<Button
-								className={cn(
-									'flex justify-start bg-transparent hover:bg-primary/10 w-full text-foreground transition-colors',
-									isPathMatch(currentURI, href)
-										? 'text-primary-foreground bg-primary hover:text-primary-foreground hover:bg-primary'
-										: ''
-								)}
-								asChild>
-								<Link href={href}>{label}</Link>
-							</Button>
-						</li>
-					))}
-				</ul>
-			</nav>
-		</aside>
-	);
+    <aside className="col-span-1 bg-background border-r">
+      <div className="p-3.5 border-b-2">
+        <h2 className="text-xl">Dashboard</h2>
+      </div>
+      <nav className="p-4">
+        <ul className="space-y-2">
+          {menuItems.map(({ href, icon, label }) => (
+            <li key={href}>
+              <Button
+                className={cn(
+                  "flex justify-start bg-transparent hover:bg-primary/10 w-full text-foreground transition-colors",
+                  isPathMatch(currentURI, href)
+                    ? "text-primary-foreground bg-primary hover:text-primary-foreground hover:bg-primary"
+                    : ""
+                )}
+                asChild
+              >
+                <Link href={href}>{label}</Link>
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
 };
 
 export default DashboardSidebar;
