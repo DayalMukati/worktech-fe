f8293d73f5509db284a6fbb6bc6b85da4c056f58
anuragids <akharwar@idssoft.com>
2024-07-01 13:09:45 +0530

    loader changes

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -3,13 +3,12 @@ import React from 'react';
 
 const Loading = () => {
 	return (
-		<main>
-			<div className='flex flex-col justify-center items-center h-screen'>
-				<div className='border-gray-900 border-t-2 border-b-2 rounded-full w-32 h-32 animate-spin'></div>
-				<p className='mt-4 text-gray-900'>Loading...</p>
-			</div>
-		</main>
-	);
+    <main>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <p className="mt-4 text-gray-900">Loading...</p>
+      </div>
+    </main>
+  );
 };
 
 export default Loading;
