f517a581bafceca2d8b8354164a1d7769c520b0b
agni-c <agnibha0016@gmail.com>
2024-07-01 21:24:13 +0530

    refactor: Remove Sidebar component from DashboardLayout and RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,22 +4,35 @@ import './globals.css';
 
 import Providers from '@/components/providers';
 import Sidebar from '@/components/sidebar';
+import { getIronSession } from 'iron-session';
+import { SessionData, sessionOptions } from '@/lib/session';
+import { cookies } from 'next/headers';
 
 export const metadata: Metadata = {
 	title: 'OneWork',
 	description: 'A project management tool for teams'
 };
 
-export default function RootLayout({
+async function getSession() {
+	const session = await getIronSession<SessionData>(
+		cookies(),
+		sessionOptions
+	);
+
+	return session;
+}
+
+export default async function RootLayout({
 	children
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const session = await getSession();
 	return (
 		<html lang='en'>
 			<body className={''}>
 				<Providers>
-					<Sidebar />
+					{session.authToken ? <Sidebar /> : null}
 					{children}
 				</Providers>
 			</body>
