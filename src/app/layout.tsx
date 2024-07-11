40dc2ba232d9eca42c55df62bcffa6944b9b9255
agni-c <agnibha0016@gmail.com>
2024-07-11 10:53:12 +0530

    Update package-lock.json and package.json to include nextjs-toploader dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,35 +7,41 @@ import Sidebar from '@/components/sidebar';
 import { getIronSession } from 'iron-session';
 import { SessionData, sessionOptions } from '@/lib/session';
 import { cookies } from 'next/headers';
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from '@/components/ui/toaster';
+
+import NextTopLoader from 'nextjs-toploader';
 
 export const metadata: Metadata = {
-  title: "OneWork",
-  description: "A project management tool for teams",
+	title: 'OneWork',
+	description: 'A project management tool for teams'
 };
 
 async function getSession() {
-  const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+	const session = await getIronSession<SessionData>(
+		cookies(),
+		sessionOptions
+	);
 
-  return session;
+	return session;
 }
 
 export default async function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  const session = await getSession();
-  console.log(session);
-  return (
-    <html lang="en">
-      <body className={""}>
-        <Providers>
-          <Sidebar />
-          {children}
-          <Toaster />
-        </Providers>
-      </body>
-    </html>
-  );
+	const session = await getSession();
+	console.log(session);
+	return (
+		<html lang='en'>
+			<body className={''}>
+				<NextTopLoader color='hsl(262, 83%, 58%)' />
+				<Providers>
+					<Sidebar />
+					{children}
+					<Toaster />
+				</Providers>
+			</body>
+		</html>
+	);
 }
